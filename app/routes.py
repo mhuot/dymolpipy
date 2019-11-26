82ba83aab189383bfa6896c900f7e608b7a13d51
Mike Huot <mhuot@mhuot.net>
2019-11-25 20:32:28 -0600

    Adding the new auto-sizing to the routes

--- app/routes.py
+++ app/routes.py
@@ -4,21 +4,91 @@ import os
 import sys
 
 def printit(mytext):
-    output = "./output"
-    length = 986
-    width = 331
-    fontsize = 300
-    img = Image.new('1', (length,length), 255) # Length is 1051 so we make a square of it and then we can rotate into a rectangle that is 1051x331
+    xdimension = 970
+    ydimension = 331
+    img = Image.new('1', (xdimension,xdimension), 255) # 148?
 
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", fontsize) # System font so easy to deal with
+    # fontsize = 100 # Three Lines
+    # fontsize = 300 # One Line
+    # fontsize = 150 # Two lines?
+    fontsizes = [ 300, 150, 100 ]
+
+    fontfile = '/System/Library/Fonts/Supplemental/Tahoma.ttf'
+    # fontfile = '/usr/share/fonts/truetype/tahoma.ttf'
+    width = 0
+    printable = ""
+    # labelContent = fill("12345678901234567890123456789012345678901234567890", 17)
+    labelContent = "123456"
+    # labelContent = "First second third fouth fifth Sixth Seventh eighth nineth tenth eleventh"
+    # labelContent = "First second third fouth fifth Sixth Seventh eighth nineth tenth"
+    # labelContent = "First second third"
+    # labelContent = "Test second"
+    # labelContent = "Test"
+    labelWords = labelContent.split()
+
+    for size in fontsizes:
+        print(f"Checking size {size}")
+        font = ImageFont.truetype(fontfile, size)
+        lines = []
+        fits = False
+        linenumber = 0
+        for word in labelWords:
+            # print(f"Checking on word {word}")
+            if lines: 
+                # print(f"Let's test adding {word} to existing line {linenumber} that has '{lines[linenumber]}'")
+                currentline = lines[linenumber] + " " + word
+            else: 
+                # print(f"Cool! First word on this label will be {word}")
+                currentline = word
+            currentdim = font.getsize(currentline)[0]
+            # print(f"After adding {word}, the current line dimension would be {currentdim} the max is {xdimension}")
+            if  currentdim < xdimension:
+                # print(f"We had room at line {linenumber} for {word}")
+                fits = True
+                if lines:
+                    # print(f"Cool, we already have lines!")
+                    # print(f"Updating line {linenumber} from '{lines[linenumber]}' to '{currentline}'")
+                    lines[linenumber] = currentline
+                else:
+                    # print(f"No lines yet, let's add {currentline}")
+                    lines.append(currentline)
+            else:
+                # print(f"Adding {word} to {currentline} exceeded the max.")
+                if size == 100 and linenumber < 2:
+                    # print(f"Line {linenumber} - {lines[linenumber]}")
+                    linenumber += 1
+                    lines.append(word)
+                    # print(f"Line {linenumber} - {lines[linenumber]}")
+                    currentline = word
+                    # print(f"Start line {linenumber} for size {size} current line is now '{currentline}'")
+                    fits = True
+                elif size == 150 and linenumber < 1:
+                    # print(f"Line {linenumber} - {lines[linenumber]}")
+                    linenumber += 1
+                    lines.append(word)
+                    # print(f"Line {linenumber} - {lines[linenumber]}")
+                    currentline = word
+                    # print(f"Start line {linenumber} for size {size} current line is now '{currentline}'")
+                    fits = True
+                else:
+                    fits = False
+        if fits:
+            print(f"We have a perfect fit at {size}")
+            break
+
+    if len(lines) > 1:
+        mytext = "\n".join(lines)
+    else:
+        mytext = lines[0]
 
     d = ImageDraw.Draw(img)
     d.text((0,0), mytext, font=font)
     rotated = img.rotate(270)
-    rotated = rotated.crop( (length-width, 0, length, length) ) # Getting to the 1051x331 rectangle
+    rotated = rotated.crop( (xdimension-ydimension, 0, xdimension, xdimension) )
 
-    rotated.save(f"{output}/rotated.png")
-    os.system(f"lpr -o ppi=300 {output}/rotated.png")
+    rotated.save('./output/rotated.png')
+    img.save('./output/image.png', fillcolor="white")
+    os.system(f"lpr -o ppi=300 ./output/rotated.png")
 
 @app.route('/')
 @app.route('/index')
